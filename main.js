428e50eedf40a8f144f5cd327a873cebb77cd882
Aaron <gnhon1@outlook.com>
2020-11-14 18:38:07 +1100

    Removed node_modules dir

--- main.js
+++ main.js
@@ -2,9 +2,31 @@ const Discord = require("discord.js");
 
 const client = new Discord.Client();
 
+const prefix = '!';
+
+const fs = require('fs');
+
+client.commands = new Discord.Collection();
+
+const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+    client.commands.set(command.name, command);
+}
+
 client.once('ready', () => {
     console.log("Online!");
-})
+});
+
+client.on('message', message => {
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+
+    client.commands.get(command).execute(message, args);
+});
 
 
-client.login("Nzc3MDAzMjcyOTIxODA4OTE2.X69GsA.H24rMUPsB9SolQm-0R5ApWIdzsU");
+
+client.login("Nzc3MDAzMjcyOTIxODA4OTE2.X69GsA.ngUcdowSsHnQt5ywZJbXrbdAKpU");

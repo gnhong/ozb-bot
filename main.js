60aeb667b49564d07c6c4bee8591877d6df48291
Aaron <hongnghigia@gmail.com>
2020-11-14 13:57:44 +1100

    Initial commit

---
+++ main.js
@@ -0,0 +1,10 @@
+const Discord = require("discord.js");
+
+const client = new Discord.Client();
+
+client.once('ready', () => {
+    console.log("Online!");
+})
+
+
+client.login("Nzc3MDAzMjcyOTIxODA4OTE2.X69GsA.H24rMUPsB9SolQm-0R5ApWIdzsU");
